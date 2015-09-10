c8212b28608cbdc6b3f327422402b9b2126b7086
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2015-09-10 17:55:50 +0900

    Initial Commit

---
+++ PressTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PressTest
+//
+//  Created by iPhone on 2015/09/10.
+//  Copyright © 2015年 tatsuno system. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PressTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PressTest
+//
+//  Created by iPhone on 2015/09/10.
+//  Copyright © 2015年 tatsuno system. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
