2151a9c9a7c75ac6b09b6b5cb17acf5c7bd69d9e
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2015-09-25 17:44:25 +0900

    force touch with five UIProgressViews

--- PressTest/ViewController.m
+++ PressTest/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSMutableArray * hashArray;
+}
 
 @end
 
@@ -16,6 +18,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    hashArray = [NSMutableArray new];
+    
+    self.view.multipleTouchEnabled = YES;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +29,35 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    for (UITouch *touch in touches) {
+        [hashArray addObject:[NSNumber numberWithUnsignedLong:[touch hash]]];
+    }
+}
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    for (UITouch *touch in touches) {
+        NSInteger i = [hashArray indexOfObject:[NSNumber numberWithUnsignedLong:[touch hash]]];
+        UIProgressView * progressView = progressViewCollection[i];
+        [progressView setProgress:0];
+        for(NSInteger j = i + 1; j < 4; j++){
+            UIProgressView * nextProgressView = progressViewCollection[j + 1];
+            [progressViewCollection[j] setProgress: nextProgressView.progress];
+        }
+        [progressViewCollection[4] setProgress:0];
+    }
+    for (UITouch *touch in touches) {
+        [hashArray removeObject:[NSNumber numberWithUnsignedLong:[touch hash]]];
+    }
+}
+-(void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self touchesEnded:touches withEvent:event];
+}
+-(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    for (UITouch *touch in touches) {
+        NSInteger i = [hashArray indexOfObject:[NSNumber numberWithUnsignedLong:[touch hash]]];
+        UIProgressView * progressView = progressViewCollection[i];
+        [progressView setProgress:touch.force / touch.maximumPossibleForce];
+        NSLog(@"%ld %f",i, touch.force);
+    }
+}
 @end
